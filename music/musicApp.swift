b6793b6071445836785847d2401724e3e820e55e
Giuseppe Setem <giuseppe7d@gmail.com>
2023-06-06 14:10:14 -0300

    Initial Commit

---
+++ music/musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicApp.swift
+//  music
+//
+//  Created by Giuseppe Davanzo on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
