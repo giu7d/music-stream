284cb2327a86c0a1d91c57816219aa3213bdcc2b
Giuseppe Setem <giuseppe7d@gmail.com>
2023-06-06 14:49:44 -0300

    feat: add login screen

--- music/ContentView.swift
+++ music/ContentView.swift
@@ -8,15 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        NavigationStack {
-            List {
-                Text("Item 1")
-                Text("Item 2")
-                Text("Item 3")
+        NavigationView {
+            ZStack {
+                Color.gray.ignoresSafeArea()
+                Circle().scale(2).foregroundColor(.white.opacity(0.25))
+                Circle().scale(1.8).foregroundColor(.white.opacity(0.5))
+                Circle().scale(1.6).foregroundColor(.white.opacity(0.75))
+                
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    TextField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+//                        Authenticate User
+                    }
+                        .foregroundColor(.white)
+                        .frame(width: 300, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                    
+                    NavigationLink(
+                        destination: Text("You are logged in @\(username)"),
+                        isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
             }
-            .navigationTitle("Menu")
-        }
+        }.navigationBarHidden(true)
     }
 }
 
